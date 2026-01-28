15ffcc8ad862c57da7e44647e9850ee3ba19ee98
chooksuspect <60366182+HackCocaine@users.noreply.github.com>
2026-01-28 09:37:33 -0300

    test

--- eccentric-equator/astro.config.mjs
+++ eccentric-equator/astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hackfluencylabs.github.io',
+  site: 'https://www.hackfluency.com',
   base: '/hackfluency.com',
 });
