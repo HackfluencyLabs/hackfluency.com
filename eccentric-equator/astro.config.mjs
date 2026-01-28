9870a5f5603168d72db043ac5a475cc504a8701e
chooksuspect <60366182+HackCocaine@users.noreply.github.com>
2026-01-28 08:59:02 -0300

    fix css

--- eccentric-equator/astro.config.mjs
+++ eccentric-equator/astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hackcocaine.github.io',
-  base: '/special-palm-tree',
+  site: 'https://hackfluencylabs.github.io',
+  base: '/hackfluency.com',
 });
